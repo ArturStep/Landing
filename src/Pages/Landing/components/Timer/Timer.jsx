da32db7f5d3ee611f60e18443d25670539b6414d
ArturStep <www.69988844@gmail.com>
2022-11-20 12:46:44 +0300

    feat: refactoring

--- src/Pages/Landing/components/Timer/Timer.jsx
+++ src/Pages/Landing/components/Timer/Timer.jsx
@@ -3,17 +3,19 @@ import {useEffect, useState} from "react";
 import s from './Timer.module.scss'
 
 const Timer = ({timerActive, setTimerActive}) => {
-  const [seconds, setSeconds] = useState(60);
+  const [second, setSeconds] = useState(60);
 
-  const minutes = parseInt(seconds / 60, 10);
+  const minutes = Math.floor(second / 60);
+
+  const seconds = second % 60;
 
   useEffect(() => {
-    if (seconds > 0 && timerActive) {
-      setTimeout(setSeconds, 1000, seconds - 1);
+    if (second > 0 && timerActive) {
+      setTimeout(setSeconds, 1000, second - 1);
     } else {
       setTimerActive(false);
     }
-  }, [seconds, timerActive]);
+  }, [second, timerActive]);
 
   return (
     <>
